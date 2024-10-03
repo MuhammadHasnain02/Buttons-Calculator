71bc5b36fb07262dd93fd203026b2b8393db26a1
Muhammad Hasnain <mrhasnain9009@gmail.com>
2024-10-02 20:49:26 -0700

    Add files via upload

---
+++ calculator-buttons.js
@@ -0,0 +1,16 @@
+function buttons(e){
+document.getElementById('inp').value += e
+}
+
+function eq(){
+var a = document.getElementById('inp')
+a.value = eval(a.value)
+}
+
+function clr(){
+document.getElementById('inp').value = ''
+}
+function clr2(){
+var b = document.getElementById('inp').pop
+
+}
